11a2e389fdac3161cfe1dd9c336ae906c1a0fd51
Thales Pereira <thalespereiradejesus@gmail.com>
2021-11-11 21:33:50 -0300

    Fix test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  URL_BASE: 'http://ec2-18-219-104-49.us-east-2.compute.amazonaws.com:8000',
-  URL_BASE_CRAWLER:
-    'http://ec2-18-216-33-231.us-east-2.compute.amazonaws.com:8000',
+  URL_BASE: '/calculator-api',
+  URL_BASE_CRAWLER: '/scraping-api',
 };
